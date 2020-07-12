443e8b910c994f9778d18c9a69b10237948b95c2
vagrant <vagrant@localhost.localdomain>
2020-07-12 14:51:26 +0000

    fix-schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_07_08_075359) do
-
+ActiveRecord::Schema.define(version: 2020_07_10_130008) do
 
   create_table "comments", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -41,7 +39,6 @@ ActiveRecord::Schema.define(version: 2020_07_08_075359) do
   end
 
   create_table "users", force: :cascade do |t|
-
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -49,9 +46,10 @@ ActiveRecord::Schema.define(version: 2020_07_08_075359) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
 
 end
