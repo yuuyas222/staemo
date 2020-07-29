0edf031b889a3d0c799c85a9bf8ccd1110e8c165
vagrant <vagrant@localhost.localdomain>
2020-07-29 07:02:30 +0000

    fix-root-login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
     before_action :set_search
 
     def after_sign_in_path_for(resource)
-        user_top_path
+        user_top_path(current_user)
     end
 
     def after_sign_out_path_for(resource)
