677173ca7e263eb8b77156940466a8fc81b663f0
vagrant <vagrant@localhost.localdomain>
2020-08-23 07:06:47 +0000

    fix js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -46,6 +46,22 @@
         }
     });
 
+    // 画像投稿時、即座にプレビューされる。
+    function previewImage(id) {
+        const target = this.event.target;    
+        const file = target.files[0];      
+        const reader = new FileReader();     
+        reader.onloadend = function() {     
+          preview.src = reader.result;       
+        };
+        if (file) {
+          reader.readAsDataURL(file);       
+        } else {
+          preview.src = "";
+        }
+      }
+
+
     
 }
 
