5bc780a8051cb58e8647ac2cd0352cedf189b169
vagrant <vagrant@localhost.localdomain>
2020-08-28 08:28:14 +0000

    try user_model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
+  describe "ユーザー情報について" do
+    let(:user){ User.new(params) }
+    let(:params){ {name: current_user.name, email: current_user.email } }
+    it "新規登録ができている" do
+      expect(user).to be_vaild
+    end
+  end
 end
